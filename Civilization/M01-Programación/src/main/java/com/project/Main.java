c122abcb56d76b63e69864f3bb2428a059d1bebb
JoelMartinezVilla <JoelMartinezVilla>
2024-05-03 19:01:06 +0200

    Attack unit y clases hijas creadas
    Creada la clase padre AttackUnit y las clases hijas Swordsman, Spearman, Crossbow y Cannon

--- Civilization/M01-Programación/src/main/java/com/project/Main.java
+++ Civilization/M01-Programación/src/main/java/com/project/Main.java
@@ -1,10 +1,5 @@
 package com.project;
 
-import java.sql.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.Locale;
-
 public class Main {
 
     public static void main(String[] args) {
---
+++ Civilization/M01-Programación/src/main/java/com/project/Spearman.java
@@ -0,0 +1,11 @@
+package com.project;
+
+public class Spearman extends AttackUnit{
+    public Spearman(int armor, int baseDamage){
+        super.initialArmor = armor;
+    }
+
+    public Spearman(){
+        
+    }
+}
