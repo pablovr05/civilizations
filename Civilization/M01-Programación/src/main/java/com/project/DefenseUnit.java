c230429898be77daa1e0cab4d1001b6c4cebee35
AdriaMartinez-231 <am76635@gmail.com>
2024-05-07 19:02:06 +0200

    Cambio militaryUnit a clase abstracta

--- Civilization/M01-Programación/src/main/java/com/project/DefenseUnit.java
+++ Civilization/M01-Programación/src/main/java/com/project/DefenseUnit.java
@@ -1,6 +1,6 @@
 package com.project;
 
-public abstract class DefenseUnit implements Variables {
+public abstract class DefenseUnit extends MilitaryUnit implements Variables {
     protected int armor;
     protected int initialArmor;
     protected int baseDamage;
