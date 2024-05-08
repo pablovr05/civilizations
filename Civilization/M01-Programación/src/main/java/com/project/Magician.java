628773d8402b7e19d879aa032a1a92a1313113f6
AdriaMartinez-231 <am76635@gmail.com>
2024-05-08 16:50:22 +0200

    MiltaryUnit implementar
    Implementacion de las funciones militaryUnit en las subclasses SpecialUnit

--- Civilization/M01-Programación/src/main/java/com/project/Magician.java
+++ Civilization/M01-Programación/src/main/java/com/project/Magician.java
@@ -8,4 +8,52 @@ public class Magician extends SpecialUnit {
         super.baseDamage = BASE_DAMAGE_MAGICIAN + (PLUS_ATTACK_MAGICIAN_BY_TECHNOLOGY * civilization.getTechnologyAttack()) * 1000/100;
         super.experience = 0;
     }
+
+    public int attack(){
+        return super.baseDamage;
+    }
+
+    public void takeDamage(int receivedDamage){
+        super.armor -= receivedDamage;
+    }
+
+    public int getActualArmor(){
+        return super.armor;
+    }
+
+    public int getFoodCost(){
+        return FOOD_COST_MAGICIAN;
+    }
+
+    public int getWoodCost(){
+        return WOOD_COST_MAGICIAN;
+    }
+
+    public int getIronCost(){
+        return IRON_COST_MAGICIAN;
+    }
+
+    public int getManaCost(){
+        return MANA_COST_MAGICIAN;
+    }
+
+    public int getChanceGeneratingWaste(){
+        return CHANCE_GENERATNG_WASTE_MAGICIAN;
+    }
+
+    public int getChanceAttackAgain(){
+        return CHANCE_ATTACK_AGAIN_MAGICIAN;
+    }
+
+    public void resetArmor(){
+        super.armor = super.initialArmor;
+    }
+
+    public void setExperience(int n){
+        super.experience = n;
+    }
+
+    public int getExperience(){
+        return super.experience;
+    }
 }
--- Civilization/M01-Programación/src/main/java/com/project/Priest.java
+++ Civilization/M01-Programación/src/main/java/com/project/Priest.java
@@ -8,4 +8,52 @@ public class Priest extends SpecialUnit {
         super.baseDamage = 0;
         super.experience = 0;
     }
+
+    public int attack(){
+        return super.baseDamage;
+    }
+
+    public void takeDamage(int receivedDamage){
+        super.armor -= receivedDamage;
+    }
+
+    public int getActualArmor(){
+        return super.armor;
+    }
+
+    public int getFoodCost(){
+        return FOOD_COST_PRIEST;
+    }
+
+    public int getWoodCost(){
+        return WOOD_COST_PRIEST;
+    }
+
+    public int getIronCost(){
+        return IRON_COST_PRIEST;
+    }
+
+    public int getManaCost(){
+        return MANA_COST_PRIEST;
+    }
+
+    public int getChanceGeneratingWaste(){
+        return CHANCE_GENERATNG_WASTE_PRIEST;
+    }
+
+    public int getChanceAttackAgain(){
+        return CHANCE_ATTACK_AGAIN_PRIEST;
+    }
+
+    public void resetArmor(){
+        super.armor = super.initialArmor;
+    }
+
+    public void setExperience(int n){
+        super.experience = n;
+    }
+
+    public int getExperience(){
+        return super.experience;
+    }
 }
