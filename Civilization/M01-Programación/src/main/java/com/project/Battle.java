1027d7b9a9942cc4103cc1ae46fdccfea901f3f1
JoelMartinezVilla <JoelMartinezVilla>
2024-05-10 18:36:53 +0200

    funciones añadidas a battle

--- Civilization/M01-Programación/src/main/java/com/project/Battle.java
+++ Civilization/M01-Programación/src/main/java/com/project/Battle.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 
 
 public class Battle {
-    ArrayList<ArrayList<MilitaryUnit>> civilizationArmy;
-    ArrayList<ArrayList<MilitaryUnit>> enemyArmy;
-    ArrayList<MilitaryUnit> armies;
-    String battleDevelopment;
-    int initialCostFleet;
-    int initialNumberUnitsCivilization;
-    int initialNumberUnitsEnemy;
+    ArrayList<ArrayList<MilitaryUnit>> civilizationArmy;//
+    ArrayList<ArrayList<MilitaryUnit>> enemyArmy;//
+    ArrayList<ArrayList<ArrayList<MilitaryUnit>>> armies;//
+    String battleDevelopment;//
+    ArrayList<int[]> initialCostFleet;//
+    int initialNumberUnitsCivilization;//
+    int initialNumberUnitsEnemy;//
     int wasteWoodIron;
     int enemyDrops;
     int civilizationDrops;
@@ -22,6 +22,54 @@ public class Battle {
     public Battle(ArrayList<ArrayList<MilitaryUnit>> civilizationArmy, ArrayList<ArrayList<MilitaryUnit>> enemyArmy ){
         this.civilizationArmy = civilizationArmy;
         this.enemyArmy = enemyArmy;
+        this.armies = new ArrayList<>();
+        this.armies.add(civilizationArmy);
+        this.armies.add(enemyArmy);
+        this.battleDevelopment = "";
+        this.initialCostFleet = getInitialCostFleet();
+        initialNumberUnitsCivilization = getArmyQuantity(civilizationArmy);
+        initialNumberUnitsEnemy = getArmyQuantity(enemyArmy);
+    }
+
+    public int getArmyQuantity(ArrayList<ArrayList<MilitaryUnit>> army){
+        int suma = 0;
+        for(int i = 0; i<army.size(); i++){
+            suma += army.get(i).size();
+        }
+        
+        return suma;
+    }
+
+    public ArrayList<int[]> getInitialCostFleet(){
+        ArrayList<int[]> initialCostFleet = new ArrayList<>(); 
+        int[] lista = new int[3];
+        int comida = 0;
+        int madera = 0;
+        int hierro = 0;
+        for(int i = 0; i<civilizationArmy.size(); i++){
+            comida += civilizationArmy.get(i).size()*Variables.FOOD_COST_UNITS[i];
+            madera += civilizationArmy.get(i).size()*Variables.WOOD_COST_UNITS[i];
+            hierro += civilizationArmy.get(i).size()*Variables.IRON_COST_UNITS[i];
+        }
+        lista[0] = comida;
+        lista[1] = madera;
+        lista[2] = hierro;
+        initialCostFleet.add(lista);
+
+        lista = new int[3];
+        comida = 0;
+        madera = 0;
+        hierro = 0;
+        for(int i = 0; i<enemyArmy.size(); i++){
+            comida += enemyArmy.get(i).size()*Variables.FOOD_COST_UNITS[i];
+            madera += enemyArmy.get(i).size()*Variables.WOOD_COST_UNITS[i];
+            hierro += enemyArmy.get(i).size()*Variables.IRON_COST_UNITS[i];
+        }
+        lista[0] = comida;
+        lista[1] = madera;
+        lista[2] = hierro;
+        initialCostFleet.add(lista);
+        return initialCostFleet;
     }
 
     String getBattleReport(int battles){
