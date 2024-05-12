b757796bd8934013af9e4feac8e60e46e673dd00
pablovr05 <152628347+pablovr05@users.noreply.github.com>
2024-05-12 17:42:14 +0200

    hot fix

--- Civilization/M01-Programación/src/main/java/com/project/Main.java
+++ Civilization/M01-Programación/src/main/java/com/project/Main.java
@@ -3,14 +3,10 @@ package com.project;
 public class Main {
 
     public static void main(String[] args) {
-        addCivlization_stats("Civilizacion de Jose");
-    }
-
-    public static void addCivlization_stats(String name){
         AppData db = AppData.getInstance();
-        String sql = "INSERT INTO Civilization_stats (name,wood_amount,iron_amount,food_amount,mana_amount,magicTower_counter,church_counter,farm_counter,smithy_counter,carpentry_counter,technology_defense_level,technology_attack_level,battles_counter) VALUES ('" + name + "',10000,10000,10000,10000,0,0,0,0,0,0,0,0)";
-        db.update(sql);
-        System.out.println(db.query("SELECT * FROM Civilization_stats"));
+        db.delCivlizations();
+        System.out.println(db.addCivilization("Civilización_de_pablo"));
+        System.out.println(db.addCivilization("Civilización_de_pablo"));
+        System.out.println(db.getIdByName("Civilización_de_pablo"));
     }
 }
-
