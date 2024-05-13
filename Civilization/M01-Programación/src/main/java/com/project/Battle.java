4b75960fa001ade46ac2f87856a8d6fe59fbd988
JoelMartinezVilla <JoelMartinezVilla>
2024-05-13 17:05:23 +0200

    battle casi acabado

--- Civilization/M01-Programación/src/main/java/com/project/Battle.java
+++ Civilization/M01-Programación/src/main/java/com/project/Battle.java
@@ -14,8 +14,8 @@ public class Battle {
     int wasteWoodIron;
     int enemyDrops;
     int civilizationDrops;
-    int resourcesLooses;
-    int initialArmies;
+    ArrayList<int[]> resourcesLooses;//
+    ArrayList<int[]> initialArmies;//
     int[] actualNumberUnitsCivilization;//
     int[] actualNumberUnitsEnemy;//
 
@@ -31,6 +31,7 @@ public class Battle {
         this.initialNumberUnitsEnemy = getArmyQuantity(enemyArmy);
         this.actualNumberUnitsCivilization = getArrayQuantities(civilizationArmy);
         this.actualNumberUnitsEnemy = getArrayQuantities(enemyArmy);
+        initInitialArmies();
     }
 
     public int getArmyQuantity(ArrayList<ArrayList<MilitaryUnit>> army){
@@ -87,15 +88,45 @@ public class Battle {
     }
 
     String getBattleDevelopment(){
-        return "";
+        return this.battleDevelopment;
     }
 
     void initInitialArmies(){
-
+        this.initialArmies.add(getArrayQuantities(civilizationArmy));
+        this.initialArmies.add(getArrayQuantities(enemyArmy));
     }
 
     void updateResourcesLooses(){
+        resourcesLooses = new ArrayList<>();
+        int[] loss = new int[4];
+        int madera = 0;
+        int comida = 0;
+        int hierro = 0;
+        for(int i = 0; i<civilizationArmy.size(); i++){
+            madera += Variables.WOOD_COST_UNITS[i]*(initialArmies.get(0)[i]-civilizationArmy.get(i).size());
+            comida += Variables.FOOD_COST_UNITS[i]*(initialArmies.get(0)[i]-civilizationArmy.get(i).size());
+            hierro += Variables.IRON_COST_UNITS[i]*(initialArmies.get(0)[i]-civilizationArmy.get(i).size());
+        }
+        loss[0] = comida;
+        loss[1] = madera;
+        loss[2] = hierro;
+        loss[3] = comida*10+madera*5+hierro;
+        this.resourcesLooses.add(loss);
 
+        loss = new int[4];
+        madera = 0;
+        comida = 0;
+        hierro = 0;
+        for(int i = 0; i<enemyArmy.size(); i++){
+            madera += Variables.WOOD_COST_UNITS[i]*(initialArmies.get(1)[i]-civilizationArmy.get(i).size());
+            comida += Variables.FOOD_COST_UNITS[i]*(initialArmies.get(1)[i]-civilizationArmy.get(i).size());
+            hierro += Variables.IRON_COST_UNITS[i]*(initialArmies.get(1)[i]-civilizationArmy.get(i).size());
+        }
+        loss[0] = comida;
+        loss[1] = madera;
+        loss[2] = hierro;
+        loss[3] = comida*10+madera*5+hierro;
+        this.resourcesLooses.add(loss);
     }
 
     int fleetResourceCost(ArrayList<MilitaryUnit> army) {
@@ -106,23 +137,76 @@ public class Battle {
         return 0;
     }
 
-    int remainderPercentageFleet(ArrayList<MilitaryUnit> army){
-        return 0;
+    int remainderPercentageFleet(ArrayList<ArrayList<MilitaryUnit>> army){
+        int remainingArmy = getArmyQuantity(army);
+        int size = army.size();
+        if(size == 9){
+            return initialNumberUnitsCivilization/remainingArmy*100;
+        }
+        return initialNumberUnitsEnemy/remainingArmy*100;
     }
 
-    int getGroupDefender(ArrayList<MilitaryUnit> army){
-        return 0;
+    int getGroupDefender(ArrayList<ArrayList<MilitaryUnit>> army){
+        int total = 0;
+        int random = (int)(Math.random()*100);
+
+        for(int i = 0; i<4; i++){
+            total += actualNumberUnitsCivilization[i];
+        }
+
+        int swordsman = 100*actualNumberUnitsCivilization[0]/total;
+        int spearman = swordsman + 100*actualNumberUnitsCivilization[1]/total;
+        int crossbow = spearman + 100*actualNumberUnitsCivilization[2]/total;
+        if(random<=swordsman){
+            return 0;
+        } else if(random<=spearman){
+            return 1;
+        } else if(random<=crossbow){
+            return 2;
+        } else{
+            return 3;
+        }
     }
 
     int getCivilizationGroupAttacker(){
-        return 0;
+        int random = (int)(Math.random()*100);
+        if(random<=4){
+            return 0;
+        } else if(random<=13){
+            return 1;
+        } else if(random<=26){
+            return 2;
+        } else if(random<=63){
+            return 3;
+        } else if(random<=67){
+            return 4;
+        } else if(random<=76){
+            return 5;
+        } else if(random<=90){
+            return 6;
+        } else{
+            return 7;
+        }
     }
     
     int getEnemyGroupAttacker(){
-        return 0;
+        int random = (int)(Math.random()*100);
+        if(random<=10){
+            return 0;
+        } else if(random<=30){
+            return 1;
+        } else if(random<=60){
+            return 2;
+        } else {
+            return 3;
+        }
     }
 
     void resetArmyArmor(){
-
+        for(int i = 0; i<civilizationArmy.size(); i++){
+            for(MilitaryUnit tropa : civilizationArmy.get(i)){
+                tropa.resetArmor();
+            }
+        }
     }
 }
